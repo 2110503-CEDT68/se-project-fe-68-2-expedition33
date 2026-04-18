5e8046889c36aebf1d6243d32078b931dc9231a4
MaxzyMVT <148692733+MaxzyMVT@users.noreply.github.com>
2026-04-18 22:46:37 +0700

    style: TopMenuItem dynamically change text by user role

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -10,6 +10,25 @@ import SignOutPanel from "@/components/modals/SignOutPanel";
 export default function TopMenu() {
     const { data: session } = useSession();
     const [showSignOut, setShowSignOut] = useState(false);
+    const role = session?.user?.role;
+    
+    let bookingMenuText;
+    if (role === "admin") {
+        bookingMenuText = "All Session"
+    } else if (role === "company") {
+        bookingMenuText = "Booked Session"
+    } else {
+        bookingMenuText = "My Session"
+    }
+
+    let profileMenuText;
+    if (role === "admin") {
+        profileMenuText = "Admin"
+    } else if (role === "company") {
+        profileMenuText = "Manage"
+    } else {
+        profileMenuText = "Profile"
+    }
 
     return (
         <>
@@ -37,7 +56,7 @@ export default function TopMenu() {
                                 icon={<svg className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2"><path strokeLinecap="round" strokeLinejoin="round" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" /></svg>}
                             />
                             <TopMenuItem 
-                                title="My Session" 
+                                title={ bookingMenuText }
                                 pageRef="/bookings" 
                                 icon={<svg className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2"><path strokeLinecap="round" strokeLinejoin="round" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" /></svg>}
                             />
@@ -48,7 +67,7 @@ export default function TopMenu() {
                         {session ? (
                             <div className="flex items-center gap-4 md:gap-6 shrink-0 ml-auto">
                                 <TopMenuItem 
-                                    title="Profile" 
+                                    title={ profileMenuText }
                                     pageRef="/api/auth/profile" 
                                     icon={<svg className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2"><path strokeLinecap="round" strokeLinejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" /></svg>}
                                 />
